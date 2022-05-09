61761341f74b76cdac2047804873ff02379bb4ea
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-05-09 17:55:40 -0300

    Update PDF.php

--- app/Models/Preview/PDF.php
+++ app/Models/Preview/PDF.php
@@ -90,6 +90,45 @@ class PDF extends Model
             $Files = new \App\Models\Preview\Files();
             $files = $Files->download();
             echo $files;
+
+            $sx = '';
+            $sx .= '
+            <!doctype html> 
+            <html lang="en">
+            <head>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <title>My PDF Viewer</title>
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.min.js"></script>              
+            </head>
+            <body>
+                 <div id="my_pdf_viewer">
+                    <div id="canvas_container">
+                        <canvas id="pdf_renderer"></canvas>
+                    </div>
+             
+                    <div id="navigation_controls">
+                        <button id="go_previous">Previous</button>
+                        <input id="current_page" value="1" type="number"/>
+                        <button id="go_next">Next</button>
+                    </div>
+             
+                    <div id="zoom_controls">  
+                        <button id="zoom_in">+</button>
+                        <button id="zoom_out">-</button>
+                    </div>
+                </div>
+            </body>
+            <script>
+                var myState = {
+                    pdf: null,
+                    currentPage: 1,
+                    zoom: 1
+                }            
+                // more code here
+            </script>            
+            </html>';
+            $sx .= 'https://code.tutsplus.com/tutorials/how-to-create-a-pdf-viewer-in-javascript--cms-32505';
             exit;
             
             $sx = '';
