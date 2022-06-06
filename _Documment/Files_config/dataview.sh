32317e9fb3093d2b6118d07fe404e27f3375e0c0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-06-06 08:30:52 -0300

    v0,22,06,06

--- _Documment/Files_config/dataview.sh
+++ _Documment/Files_config/dataview.sh
@@ -1,13 +1,28 @@
-echo "List Tools"
-curl http://localhost:8080/api/admin/externalTools | jq
-
-if [$1 = "install"]; then
-    if [$2 != ""]; then
+clear
+echo "L i s t   T o o l s"
+echo "by RDPBrasil - renefgj@gmail.com"
+echo "==========================================================================="
+if [ "$1" == "install" ]; then
+    if [ "$2" != "" ]; then
         echo "Install $1"
-        curl -X POST -H 'Content-type: application/json' http://localhost:8080/api/admin/externalTools --upload-file $1.json
+        if [ "$2" ]; then
+                curl -X POST -H 'Content-type: application/json' http://localhost:8080/api/admin/externalTools --upload-file "$2".json
+        else
+                echo "packeg not informed, ex: dataview install pdf"
+        fi
     else
         echo "File not informed, ex: pdf"
     fi
-else 
-    echo "use: dataview install <file>"
+else
+        if [ "$1" == 'list' ]; then
+                curl http://localhost:8080/api/admin/externalTools | jq
+        else
+                if [ "$1" == 'delete' ];then
+                    curl -X DELETE http://localhost:8080/api/admin/externalTools/"$2"
+                else
+                    echo "use: dataview install <file>"
+                fi
+        fi
 fi
+echo " ";
+echo " ";
