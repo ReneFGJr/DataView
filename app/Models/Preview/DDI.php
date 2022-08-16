beb59367ee98818d577eec0d361071a2bae0aea1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-08-15 23:51:34 -0300

    v0.22.08.15

--- app/Models/Preview/DDI.php
+++ app/Models/Preview/DDI.php
@@ -56,12 +56,10 @@ class DDI extends Model
 
         /**** */
         //$SERVER_URL = 'http://localhost:8080';
-        echo $SERVER_URL . '<hr>';
 
-        $file = $SERVER_URL . '/api/datasets/export?exporter=ddi&persistentId='. $PERSISTENT_ID;
+        $file = $SERVER_URL . '/api/datasets/export?exporter=ddi&persistentId=' . $PERSISTENT_ID;
         $file = 'https://vitrinedadosabertos.rnp.br/api/datasets/export?exporter=ddi&persistentId=doi%3A10.34841/rnp/RILP3P';
         $file = $SERVER_URL . '/api/datasets/export?exporter=ddi&persistentId=' . $PERSISTENT_ID;
-        echo $file;
 
         if (strlen($API_TOKEN) > 0) {
             $file .= '?key=' . $API_TOKEN;
@@ -70,49 +68,59 @@ class DDI extends Model
         $file = $Cache->download($file);
         $xml = (array)simplexml_load_file($file);
 
-        pre($xml);
-
         $sx = '';
         $sx .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">' . chr(13);
         $sx .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>' . chr(13);
         $sx .= '' . chr(13);
-        if (isset($xml['dataDscr']))
-            {
-                $var = (array)$xml['dataDscr'];
-                $var = (array)$var['var'];
-                for ($r=0;$r < count($var);$r++)
-                    {
-                        $line = (array)$var[$r];
-                        $var_name = $line['labl'];
-                        $sx .= '<hr>';
-                        $sx .= h($var_name,4);
+        if (isset($xml['dataDscr'])) {
+            $var = (array)$xml['dataDscr'];
+            $var = (array)$var['var'];
+            for ($r = 0; $r < count($var); $r++) {
+                $line = (array)$var[$r];
+                $var_name = $line['labl'];
 
-                        /********************* Variáveis */
-                        if (isset($line['sumStat']))
-                        {
-                        $sumStat = (array)$line['sumStat'];
-                        $sx .= '<table width="100%" style="border: 1px solid #000;">';
-                        $sa = '';
-                        $sb = '';
-                        $sz = round(100 / 7);
-                        for ($z=0;$z <= 7;$z++)
-                            {
-                                $sa .= '<th width="' . $sz . '" style="font-size: 50%">' . lang('dataview.sumSata_' . $z) . '</th>';
-                                $vlr = $sumStat[$z];
-                                if ($pos = strpos($vlr,'.'))
-                                    {
-                                        $vlr = substr($vlr,0,$pos+3);
-                                    }
-                                $sb .= '<td style="text-align: center" >' . $vlr . '</td>';
-                            }
-                        $sx .= '<tr>'.$sa.'</tr>';
-                        $sx .= '<tr>' . $sb . '</tr>';
-                        $sx .= '</table>';
-                        } else {
-                            //pre($line);
-                        }
+                $attr = (array)$line['@attributes'];
+                $sx .= '<hr>';
+                $sx .= 'Variable Name: '.$attr['name'].' ('. $attr['ID'].')';
+
+                $form = (array)$line['varFormat'];
+                $form = (array)$form['@attributes'];
+                $sx .= '<br>';
+                $sx .= 'Form type: <i>'.$form['type']. '</i> ';
+
+                if (isset($form['category']))
+                    {
+                        $sx .= '- Category: <i>'.$form['category']. '</i> ';
+                        $sx .= ' - Format: <i>' . $form['formatname'] . '</i> ';
                     }
+
+
+                if (isset($line['sumStat'])) {
+                    $sx .= h($var_name, 6);
+
+                    /********************* Variáveis */
+
+                    $sumStat = (array)$line['sumStat'];
+                    $sx .= '<table width="100%" style="border: 1px solid #000;">';
+                    $sa = '';
+                    $sb = '';
+                    $sz = round(100 / 7);
+                    for ($z = 0; $z <= 7; $z++) {
+                        $sa .= '<th width="' . $sz . '" style="font-size: 50%">' . lang('dataview.sumSata_' . $z) . '</th>';
+                        $vlr = $sumStat[$z];
+                        if ($pos = strpos($vlr, '.')) {
+                            $vlr = substr($vlr, 0, $pos + 3);
+                        }
+                        $sb .= '<td width="12%" style="text-align: center; border: 1px solid #000;" >' . $vlr . '</td>';
+                    }
+                    $sx .= '<tr>' . $sa . '</tr>';
+                    $sx .= '<tr>' . $sb . '</tr>';
+                    $sx .= '</table>';
+                } else {
+                    //pre($line);
+                }
             }
+        }
         return $sx;
     }
 }
