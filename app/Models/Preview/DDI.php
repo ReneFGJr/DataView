809596bf86d92097d4d9570f0009debe54f46167
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2022-09-28 16:55:49 -0300

    Update DDI.php

--- app/Models/Preview/DDI.php
+++ app/Models/Preview/DDI.php
@@ -68,7 +68,7 @@ class DDI extends Model
 
         $file = $Cache->download($file);
         $xml = (array)simplexml_load_file($file);
-
+//pre($xml);
         $sx = '';
         $sx .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">' . chr(13);
         $sx .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>' . chr(13);
