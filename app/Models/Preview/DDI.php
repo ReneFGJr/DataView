18f96f4b170270ed25f2de9c216ec9b9449b141d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-12-06 10:15:52 -0300

    Update DDI.php

--- app/Models/Preview/DDI.php
+++ app/Models/Preview/DDI.php
@@ -345,12 +345,12 @@ class DDI extends Model
         $sx .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">' . chr(13);
         $sx .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>' . chr(13);
         $sx .= '' . chr(13);
+        pre($xml, false);
         if (isset($xml['dataDscr'])) {
             $var = (array)$xml['dataDscr'];
             $var = (array)$var['var'];
             for ($r = 0; $r < count($var); $r++) {
                 $line = (array)$var[$r];
-                pre($line,false);
                 $var_name = $line['labl'];
 
                 /************************************************* @ATTRIBUTeS */
